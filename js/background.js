66f0cf0d5b92d0776499fc0b6efc3e84523fa550
Guest <guest-nwynmf@h28638.sgb.skir.kr>
2021-02-28 23:47:18 +0900

    add UNSPLASH_API

--- js/background.js
+++ js/background.js
@@ -1,5 +1,6 @@
 // body에 사진을 삽입할 것임
 // 랜덤 난수를 생성해서 그 난수에 해당하는 번호의 사진을 랜덤 출력하게 할 것임
+const UNSPLASH_API_KEY = "cD9Hl1zVQ3FdRlgsB2kQQi33AdTeTvt8DXcW1Tx_67Q";
 
 const body = document.querySelector("body");
 
---
+++ js/background_api.js
@@ -0,0 +1,60 @@
+const UNSPLASH_API_KEY = "cD9Hl1zVQ3FdRlgsB2kQQi33AdTeTvt8DXcW1Tx_67Q";
+const UNSPLASH_URL = `https://api.unsplash.com/photos/random/?client_id=${UNSPLASH_API_KEY}&query=landscape&orientation=landscape`;
+
+const body = document.querySelector("body");
+const locationContainer = document.querySelector(".js-location");
+
+function loadBackground() {
+    const savedImage = localStorage.getItem("background");
+    if (savedImage === null) {
+        getBackground();
+    }
+    else {
+        const parsedImage = JSON.parse(savedImage);
+        locationContainer.innerHTML = parsedImage.name;
+        const img = new Image();
+        img.src = parsedImage.url;
+        img.classList.add("backgroundImg");
+        body.prepend(img);
+    }
+}
+
+function saveBackground(url, name) {
+    const savedImage = localStorage.getItem("background");
+    if (savedImage != null) {
+        localStorage.removeItem("background");
+    }
+    const imageObj = {
+        url: url,
+        name: name
+    };
+    localStorage.setItem("background", JSON.stringify(imageObj));
+    loadBackground();
+}
+
+function getBackground() {
+    fetch(UNSPLASH_URL)
+        .then(response => response.json())
+        .then(json => {
+            const image = json;
+            if (image.urls && image.urls.regular && image.location) {
+                const imageURL = image.urls.regular;
+                const location = image.location;
+                const name = location.name;
+                saveBackground(imageURL, name);
+            }
+            else {
+                getBackground();
+            }
+        });
+}
+
+function init() {
+    const savedImage = localStorage.getItem("background");
+    if (savedImage != null) {
+        localStorage.removeItem("background");
+    }
+    loadBackground();
+}
+
+init();
